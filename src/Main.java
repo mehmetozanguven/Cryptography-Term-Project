9bc9e6e3e089b35b77e9a9ddc3d83822fbc87279
mehmetozanguven <mehmetozanguven@localhost.localdomain>
2019-06-29 01:09:51 +0300

    Main.java Changed

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,16 @@
 
+import diffie_helman_scheme.DiffieHellmanScheme;
 import dss_scheme.DSSScheme;
+import rsa_scheme.RSAScheme;
 
 
 public class Main {
 
 
     public static void main(String [] args){
-        DSSScheme.startDSSScheme();
+        //DiffieHellmanScheme.startDiffieHellmanScheme();
+        //RSAScheme.startRSAScheme();
+        //DSSScheme.startDSSScheme();
     }
 
 }
